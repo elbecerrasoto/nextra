18da20c4333bbd2186aa7e39f419cda5ae1354d9
Emanuel Becerra Soto <elbecerrasoto@gmail.com>
2023-12-27 08:33:15 -0700

    wip parse gff

--- get_neighbors.R
+++ get_neighbors.R
@@ -8,15 +8,13 @@
 # ywqL Endo V gene
 # BSUA_RS19520
 
+# ./download_genome.py -i gff3 protein genome 'seq-report' cds -p GCF_000699465.1_bsubJH642 GCF_000699465.1
 
 library(tidyverse)
 library(stringr)
 
-CDS <- microseq::readGFF("GCF_000699465.1_bsubJH642.gff") |>  filter(Type=="CDS")
+CDS <- microseq::readGFF("GCF_000699465.1/GCF_000699465.1_bsubJH642.gff") |>  filter(Type=="CDS")
 
-CDS <- 
   
 # attributes targets
-# 
-
-str_detect("hello", "llo")
+# str_detect("hello", "llo")
